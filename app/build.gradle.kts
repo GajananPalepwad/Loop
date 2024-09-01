9e674390741941953b168ba3a1ec9cc6c9be7567
GajananPalepwad <2021bcs041@sggs.ac.in>
2024-09-01 15:02:04 +0530

    new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.gn4k.loop"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
